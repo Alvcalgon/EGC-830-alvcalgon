47f5169fb3f37b7e3da683a089d67ae60febb49d
alvcalgon <alvcalgon	@alum.us.es>
2019-09-07 16:38:03 +0200

    Solucionado bug relacionado con la incidencia registrada: fixed #1

--- decide/manage.py
+++ decide/manage.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+# Solucionado error de sintaxis: Ejercicio A.05
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "decide.settings")
     try:
